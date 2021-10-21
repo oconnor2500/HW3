5de2f365d968a8b69c9d24c733047a40a1bb4037
Chris OConnor <oconnor2500@gmail.com>
2021-10-20 20:31:35 -0500

    new file with mean ht code

---
+++ midtermq3mean.R
@@ -0,0 +1,24 @@
+#retrieve BRFSS from Kim's github
+brfss2017 <- read_csv('https://raw.githubusercontent.com/kijohnson/ADA_Spring_2019/master/BRFSS2017_10percent_v2.csv')
+
+#finding median height for males and females
+
+median(brfss2017$ht_meters[brfss2017$sex == 'Male'], na.rm = TRUE)
+median(brfss2017$ht_meters[brfss2017$sex == 'Female'], na.rm = TRUE)
+
+mean(brfss2017$ht_meters[brfss2017$sex == 'Male'], na.rm = TRUE)
+mean(brfss2017$ht_meters[brfss2017$sex == 'Female'], na.rm = TRUE)
+
+
+#making graph of median height for males and females
+male.female.height <- brfss2017 %>% 
+  mutate(sex = na_if(x = sex, y = 'Refused')) %>% 
+  drop_na(sex) %>% 
+  ggplot(aes(x = sex, y = ht_meters)) +
+  geom_bar(aes(fill = ht_meters), stat = "summary", fun = median) +
+  theme_minimal() +
+  labs(x = "Sex", y = "Median Height (in meters)") 
+male.female.height
+
+ggsave(filename = "MedianHeightbySex.png",
+       width = 4, height = 4, units = "in", bg="white")
